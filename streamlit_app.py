da2e64b59a1ea3be13dfb9a35108572ae7cf49cd
Nandakumar Sathuragiri <109757058+NandaSvks@users.noreply.github.com>
2022-07-21 23:39:34 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-strealit.header('Breakfast Menu')
-strealit.text('Omega 3 & Blueberry Oat Meal')
-strealit.text('Kale, Spinach & Rocket Smoothie')
-strealit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oat Meal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
